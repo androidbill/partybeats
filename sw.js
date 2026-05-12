ad50ed765269be4feeb0d10175b49f3f2347dee5
androidbill <36899036+androidbill@users.noreply.github.com>
2026-05-12 15:59:49 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'party-beats-2026.05.12.01';
+const CACHE_NAME = 'party-beats-2026.05.12.02';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon.svg'];
 
 self.addEventListener('install', event => {
